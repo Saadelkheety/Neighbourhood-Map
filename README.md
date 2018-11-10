4b8287fe0ff25d4f746533a0f39a2730a29e4eef
Saad Elkheety <SaadElkheety@gmail.com>
2018-11-10 08:34:06 +0200

    addind streetview api

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Neighbourhood Map
+a single page application featuring a map of Faculty of engineering, Alexandria University. the app has a functionality to this map including highlighted locations, third-party data about those locations and various ways to browse the content.
+
+## frameworks and libraries
+- falsk
+- knockout.js
+- bootstrap 4
+- jquery
+## APIs
+- google maps
+- google streetview
+- foursquare
+
+## to run the app
+- download and unzip the files
+- install python and flask
+- run application.py
--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,6 @@
 // this is the data to handle the project //////
 let locations = [{
         title: 'Electra Center',
-        visible: ko.observable(true),
         location: {
             lat: 31.209072,
             lng: 29.924966
@@ -9,7 +8,6 @@ let locations = [{
     },
     {
         title: 'Sidi Gaber Station',
-        visible: ko.observable(true),
         location: {
             lat: 31.218918,
             lng: 29.942362
@@ -17,7 +15,6 @@ let locations = [{
     },
     {
         title: 'Alexandria Library',
-        visible: ko.observable(true),
         location: {
             lat: 31.208990,
             lng: 29.909199
@@ -25,7 +22,6 @@ let locations = [{
     },
     {
         title: 'Faculty of engineering Alexendria university',
-        visible: ko.observable(true),
         location: {
             lat: 31.20741,
             lng: 29.924116
@@ -33,12 +29,53 @@ let locations = [{
     },
     {
         title: 'M3ML cooworking space',
-        visible: ko.observable(true),
         location: {
             lat: 31.246797,
             lng: 29.972079
         }
     },
+    {
+        title: 'Knight Academy',
+        location: {
+            lat: 31.210663,
+            lng: 29.921678
+        }
+    },
+    {
+        title: 'Desertwood Pavillion',
+        location: {
+            lat: 31.208900,
+            lng: 29.921732
+        }
+},
+    {
+        title: 'Track Training center',
+        location: {
+            lat: 31.207171,
+            lng:  29.926630
+        }
+    },
+    {
+        title: 'ROBOTA',
+        location: {
+            lat: 31.207779,
+            lng: 29.927067
+        }
+    },
+    {
+        title: 'Emad Eldin Center',
+        location: {
+            lat: 31.208805,
+            lng: 29.925987
+        }
+    },
+    {
+        title: 'Alexandria Training Center',
+        location: {
+            lat: 31.231012,
+            lng:  29.961144
+        }
+    }
 ];
 // to store the markers of the map
 let markers = [];
@@ -62,59 +99,70 @@ function initMap() {
 function AppViewModel(map) {
     // Close infowindow when clicked elsewhere on the map
     map.addListener("click", function() {
-      largeInfowindow.close();
-      largeInfowindow.setContent('');
-      largeInfowindow.marker = null;
+        largeInfowindow.close();
+        largeInfowindow.setContent('');
+        largeInfowindow.marker = null;
     });
+    // to maintain the bound of the map
+    bounds = new google.maps.LatLngBounds();
+    // to maintain the bound upon window resize
+    window.onresize = function() {
+        map.fitBounds(bounds);
+    };
+    // create info window
     let largeInfowindow = new google.maps.InfoWindow();
     // create our dear markers on our beutiful map
     for (let i = 0; i < locations.length; i++) {
-      // Get the position from the location array.
-      let position = locations[i].location;
-      let title = locations[i].title;
-      // Create a marker per location, and put into markers array.
-      let marker = new google.maps.Marker({
-          position: position,
-          title: title,
-          label: title[0],
-          opacity: 0.8,
-          fillcolor: '#fff',
-          animation: google.maps.Animation.DROP,
-          map: map,
-          show: ko.observable(true),
-          id: i
-      });
+        // Get the position from the location array.
+        let position = locations[i].location;
+        let title = locations[i].title;
+        // Create a marker per location, and put into markers array.
+        let marker = new google.maps.Marker({
+            position: position,
+            title: title,
+            label: title[0],
+            opacity: 0.8,
+            fillcolor: '#fff',
+            animation: google.maps.Animation.DROP,
+            map: map,
+            show: ko.observable(true), // this is just for the list updating
+            id: i
+        });
 
-
-      /*marker.addListener('mouseout', function() {
-          this.setAnimation(google.maps.Animation.Null);
-      });*/
-      marker.addListener('click', toggleBounce);
-      function toggleBounce() {
+        // the map bounding according to markers
+        bounds.extend(marker.position);
+        // wken click, bounce
+        marker.addListener('click', toggleBounce);
+        function toggleBounce() {
             if (marker.getAnimation() !== null) {
-              marker.setAnimation(null);
+                marker.setAnimation(null);
             } else {
-              marker.setAnimation(google.maps.Animation.BOUNCE);
-              setTimeout(function(){marker.setAnimation(null);},500)
+                marker.setAnimation(google.maps.Animation.BOUNCE);
+                setTimeout(function() {
+                    marker.setAnimation(null);
+                }, 500)
             }
         }
+
         // Create an onclick event to open an infowindow at each marker.
         marker.addListener('click', function() {
-          populateInfoWindow(this, largeInfowindow);
+            populateInfoWindow(this, largeInfowindow);
         });
-    // Push the marker to our array of markers.
-      markers.push(marker);
-
-  }
+        // Push the marker to our array of markers.
+        markers.push(marker);
+    }
     //when an item is selected from a list
     this.selecItem = function(marker) {
-     populateInfoWindow(marker, largeInfowindow);
-     marker.setAnimation(google.maps.Animation.BOUNCE);
-     setTimeout(function(){marker.setAnimation(null);},500)
-      };
+        populateInfoWindow(marker, largeInfowindow);
+        marker.setAnimation(google.maps.Animation.BOUNCE);
+        setTimeout(function() {
+            marker.setAnimation(null);
+        }, 500)
+    };
 
-    //this.markers = ko.observableArray(markers);
+    // query which is related to the search input
     this.query = ko.observable('');
+    // the function to call foe search input
     this.filter = function() {
         let search = this.query();
         if (search.length === 0) {
@@ -134,41 +182,61 @@ function AppViewModel(map) {
             }
         }
     };
+
+    // setup the infowindow
     function populateInfoWindow(marker, infowindow) {
-          // Check to make sure the infowindow is not already opened on this marker.
-          if (infowindow.marker != marker) {
+        // Check to make sure the infowindow is not already opened on this marker.
+        if (infowindow.marker != marker) {
             // Clear the infowindow content to give the streetview time to load.
             infowindow.setContent('');
-            infowindow.setOptions({maxWidth:250});
-            infowindow.setOptions({disableAutoPan: false});
+            // set the max width
+            infowindow.setOptions({
+                maxWidth: 250
+            });
 
             infowindow.marker = marker;
             // Make sure the marker property is cleared if the infowindow is closed.
             infowindow.addListener('closeclick', function() {
-              infowindow.marker = null;
+                infowindow.marker = null;
             });
             // add foursquare api for nearby places.
             const request = new XMLHttpRequest();
+
             let apiUrl = ` https://api.foursquare.com/v2/venues/explore?client_id=URUBLCSWPB2WHXHOWJYZIZ3P0MKR3FUUYHX3V4PYPDZ2TSZH&client_secret=1JOHIYIIW1DYQSCIF35FTBOFVNTL1TO3AZZLBTOPZ1C5UBQY&v=20180323&limit=5&ll=${marker.position["lat"]()},${marker.position["lng"]()}&time=any&day=any&openNow=0&sortByDistance=1&radius=200`
-            request.open('GET', apiUrl);
+
+            request.open('GET', apiUrl, true);
+
+            request.addEventListener("error", transferFailed);
+
             request.send();
+
+            // Callback function for when request has an error
+            function transferFailed() {
+                infowindow.setContent(`<h6> ${marker.title}</h6>
+                    <div id="foursquare"> the foursqure api connections has a proplem, could you check your connection? </div>`);
+            }
+
+            // time out
+            request.ontimeout = function(e) {
+                console.error("Timeout!!")
+            }
+            
             // Callback function for when request completes
             request.onload = () => {
                 // Extract JSON data from request
                 const data = JSON.parse(request.response);
-                if(data.meta.code == 200){
+                if (data.meta.code == 200) {
                     let nearbyPlaces = data.response.groups[0].items;
-                    console.log("nearbyPlaces = ",nearbyPlaces);
                     let places = [];
-                    for(let i = 0; i < nearbyPlaces.length; i++) {
+                    for (let i = 0; i < nearbyPlaces.length; i++) {
                         let place = {
-                                category: nearbyPlaces[i].venue.categories[0].name,
-                                name: nearbyPlaces[i].venue.name,
-                                distance: nearbyPlaces[i].venue.location.distance
+                            category: nearbyPlaces[i].venue.categories[0].name,
+                            name: nearbyPlaces[i].venue.name,
+                            distance: nearbyPlaces[i].venue.location.distance
                         };
                         places.push(place);
-                    }// end for
-                    let content ='';
+                    } // end for
+                    let content = '';
                     for (let i = 0; i < places.length; i++) {
                         content += `
                             <h6>${places[i].name}</h6>
@@ -177,21 +245,30 @@ function AppViewModel(map) {
                                 <li>Distance: ${places[i].distance} Meters</li>
                             </ul>
                         `
-                    }// end for
+                    } // end for
                     infowindow.setContent(
                         `<div id="infowindow">
-                        <h5> ${marker.title} (nearby Places)</h5>
+                        <h5> ${marker.title}</h5>
+                        <img src="https://maps.googleapis.com/maps/api/streetview?size=200x100&location=${marker.position["lat"]()},${marker.position["lng"]()}&fov=90&heading=235&pitch=10&key=AIzaSyDHspQCoi3pEAzjICyNadaXDc2TkZcG-8Y&radius=200" alt="">
+                        <h6>nearby Places</h6>
                         <div id="foursquare" data-bind="foreach: places">
                         ${content}
+                        <div class="infowindow-footer">
+                        <sytong>this data was collected using foursquare API</strong>
+                        </div>
                      </div>
                      </div>`);
+                     bounds.extend(marker.position);
                 } // end if
                 else {
-                    infowindow.setContent(`<h3> ${marker.title}'</h3><div id="foursquare"> the foursqure api connections has a proplem </div>`);
+                    infowindow.setContent(`<h6> ${marker.title}</h6><div id="foursquare"> the foursqure api connections has a proplem </div>`);
                 }
             }
 
             infowindow.open(map, marker);
-          }
         }
+    }
+}
+function mapError() {
+  alert("Oops! Something went wrong. Please try again after sometime, or contact us");
 }
